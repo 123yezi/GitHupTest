7eea5d29fb75cc5dea605678c884975666c9c95a
123yezi <1558247257@qq.com>
2018-03-09 15:13:41 +0800

    My First Commit

---
+++ src/main/java/com/example/GitHupTest/GitHupTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.GitHupTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHupTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHupTestApplication.class, args);
+	}
+}
